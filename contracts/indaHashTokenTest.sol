f7842c1454caeaee6c2b17975cd9ef0c64e63e8e
sessai <sessai@zoom1000.com>
2017-10-18 11:07:37 +0200

    Initial commit

---
+++ contracts/indaHashTokenTest.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.16;
+
+import 'indaHashToken.sol';
+
+contract IndaHashTokenTest is IndaHashToken {
+
+  uint public testTime = 1;
+
+  // Events ---------------------------
+
+  event TestTimeSet(
+    uint _now
+  );
+
+  // Functions ------------------------
+
+  function indaHashTokenTest() {}
+
+  function atNow() constant
+    returns (uint)
+  {
+      return testTime;
+  }
+
+  function setTestTime(uint _t) onlyOwner
+  {
+    require( _t > testTime ); // to avoid errors during testing
+    testTime = _t;
+    TestTimeSet(_t);
+  }  
+
+}
